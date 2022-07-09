d110765f7cc57e32a9cad78e12b42d5b88ea5578
Salmaan Saeed <salsaeed999@gmail.com>
2022-07-08 20:32:30 -0400

    removed extra space

--- init.lua
+++ init.lua
@@ -42,4 +42,3 @@ vim.api.nvim_set_keymap('n', '<leader>n', ':NERDTreeFocus<CR>', { noremap = true
 vim.api.nvim_set_keymap('n', '<C-n>', ':NERDTree<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-f>', ':NERDTreeFind<CR>', { noremap = true })
-
