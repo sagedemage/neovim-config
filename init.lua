ea79a917fcabeebfd87a1001d6dfdeceb1107940
Salmaan Saeed <salsaeed999@gmail.com>
2022-09-17 00:36:05 -0400

    Fixed COC autocomplete configuration. Neovim autocompletes the code when I press enter.

--- init.lua
+++ init.lua
@@ -46,8 +46,35 @@ vim.opt.number=true
 -- hidden
 vim.opt.hidden=true
 
+-- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
+-- delays and poor user experience.
+vim.opt.updatetime=300
+
 -- COC bindings
+-- Use <c-space> to trigger completion.
 vim.api.nvim_set_keymap('i', '<c-space>', 'coc#refresh()', { silent = true, expr = true })
+
+-- Use tab for trigger completion with characters ahead and navigate.
+-- NOTE: There's always complete item selected by default, you may want to enable
+-- no select by `"suggest.noselect": true` in your configuration file.
+-- NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
+-- other plugin before putting this into your config.
+
+vim.cmd([[
+	inoremap <silent><expr> <TAB>
+    	\ coc#pum#visible() ? coc#pum#next(1) :
+    	\ CheckBackspace() ? "\<Tab>" :
+    	\ coc#refresh()
+	inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+]])
+
+-- Make <CR> to accept selected completion item or notify coc.nvim to format
+-- <C-g>u breaks current undo, please make your own choice.
+
+vim.cmd([[
+inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+                              \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+]])
 
 -- Instant Markdown Preview
 vim.g['instant_markdown_autostart'] = 0
@@ -55,10 +82,6 @@ vim.g['instant_markdown_autostart'] = 0
 -- Instant Markdown Keybinding
 vim.api.nvim_set_keymap('n', '<leader>p', ':InstantMarkdownPreview<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<leader>s', ':InstantMarkdownStop<CR>', { silent = true })
-
--- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
--- delays and poor user experience.
-vim.opt.updatetime=100
 
 -- nerdtree bindings
 vim.api.nvim_set_keymap('n', '<leader>n', ':NERDTreeFocus<CR>', { noremap = true })
