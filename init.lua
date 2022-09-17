228203ee360c7f98424e4ccdba110e57f3f2d1a4
Salmaan Saeed <salsaeed999@gmail.com>
2022-09-17 00:38:43 -0400

    formatted the vim command to my liking

--- init.lua
+++ init.lua
@@ -61,11 +61,11 @@ vim.api.nvim_set_keymap('i', '<c-space>', 'coc#refresh()', { silent = true, expr
 -- other plugin before putting this into your config.
 
 vim.cmd([[
-	inoremap <silent><expr> <TAB>
-    	\ coc#pum#visible() ? coc#pum#next(1) :
-    	\ CheckBackspace() ? "\<Tab>" :
-    	\ coc#refresh()
-	inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+inoremap <silent><expr> <TAB>
+    \ coc#pum#visible() ? coc#pum#next(1) :
+    \ CheckBackspace() ? "\<Tab>" :
+    \ coc#refresh()
+inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
 ]])
 
 -- Make <CR> to accept selected completion item or notify coc.nvim to format
